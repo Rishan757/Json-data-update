3498f6bfacf66e2833822f9e379d2d810c9a289b
Md. Rishan Ahmed <166410540+Rishan757@users.noreply.github.com>
2026-01-19 19:03:39 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+
+// link data
+const data = require("./friends_data.json");
+// console.log(data);
+
+
+// 0 number index data modify
+data.friends[0].Name = "Rishan";    // Modify data change sakib in Rishan
+console.log(data.friends[0].Name);
+
+data.friends[0].Age = 21;      // change 24 then in 21
+console.log(data.friends[0].Age); 
+
+
+// 1 number index data modify
+data.friends[1].Name = "Maliha"; // Modify data change sakib in Rishan
+console.log(data.friends[1].Name);
+
+data.friends[1].Age = 18;     // change 25 then in 18
+console.log(data.friends[1].Age);
+
+
+
+
